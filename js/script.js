7b6f7ad11716041d2e603c36d63ae1f436e318de
shingetsu-mgmt-proj <61572766+shingetsu-mgmt-proj@users.noreply.github.com>
2020-03-11 11:27:54 +0900

    20200311

--- js/script.js
+++ js/script.js
@@ -7,4 +7,36 @@ $(function(){
   $("#menu_close_btn").click(function(){
     $(".menu_sp").fadeOut();
   });
+
+  $(".faq_list").click(function(){
+    var $answer = $(this).find(".faq_answer");
+    if($answer.hasClass("faq_open")){
+      $answer.removeClass("faq_open");
+      $answer.slideUp();
+      $(this).find("span").text("+");
+    } else {
+      $answer.addClass("faq_open");
+      $answer.slideDown();
+      $(this).find("span").text("-");
+    }
+  });
+
+  $(".news_column").click(function(){
+    var $answer = $(this).find(".news_content");
+    if($answer.hasClass("news_open")){
+      $answer.removeClass("news_open");
+      $answer.slideUp();
+      $(this).find("span").text("+");
+    } else {
+      $answer.addClass("news_open");
+      $answer.slideDown();
+      $(this).find("span").text("-");
+    }
+  });
+
+});
+
+$('.slider').slick({
+    autoplay:true,
+    autoplaySpeed:5000,
 });
