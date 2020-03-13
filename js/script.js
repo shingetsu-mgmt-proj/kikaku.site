61f790a5cc6178058603886c6b85f11795629f82
shingetsu-mgmt-proj <61572766+shingetsu-mgmt-proj@users.noreply.github.com>
2020-03-13 15:15:37 +0900

    20200313_3

--- js/script.js
+++ js/script.js
@@ -41,24 +41,38 @@ $(function(){
     if($answer.hasClass("faq_open")){
       $answer.removeClass("faq_open");
       $answer.slideUp();
-      $(this).find("span").text("+");
+      $(this).find(".op_cl").text("+");
     } else {
       $answer.addClass("faq_open");
       $answer.slideDown();
-      $(this).find("span").text("-");
+      $(this).find(".op_cl").text("-");
     }
   });
+
+  $(".apply_campus").click(function(){
+    var $apply_process = $(this).find(".apply_process");
+    if($apply_process.hasClass("apply_open")){
+      $apply_process.removeClass("apply_open");
+      $apply_process.slideUp();
+      $(this).find(".op_cl").text("+");
+    } else {
+      $apply_process.addClass("apply_open");
+      $apply_process.slideDown();
+      $(this).find(".op_cl").text("-");
+    }
+  });
+
 
   $(".news_column").click(function(){
     var $answer = $(this).find(".news_content");
     if($answer.hasClass("news_open")){
       $answer.removeClass("news_open");
       $answer.slideUp();
-      $(this).find("span").text("+");
+      $(this).find(".op_cl").text("+");
     } else {
       $answer.addClass("news_open");
       $answer.slideDown();
-      $(this).find("span").text("-");
+      $(this).find(".op_cl").text("-");
     }
   });
 
